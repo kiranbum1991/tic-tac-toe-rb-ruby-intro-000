a9e541ba84550cdcf559202121ad2551c5d2cde6
Tommy Jake <tomlom1984@hotmail.co.uk>
2019-08-01 09:47:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -85,7 +85,7 @@ def play(board)
     turn(board)
   end
     if won?(board)
-      puts "Congratulations#{winner(board)}!"
+      puts "Congratulations #{winner(board)}!"
     else draw?(board)
       puts "Cat's game!"
     end
